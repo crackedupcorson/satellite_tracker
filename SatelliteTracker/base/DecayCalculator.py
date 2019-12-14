d27895d9ad3d454197677a8ed3cfef7b231ce6e1
ciaran corson <ciaran.corson@gmail.com>
2019-12-14 12:13:41 +0000

    some minor changes to start of orbit density calculation

--- SatelliteTracker/base/DecayCalculator.py
+++ SatelliteTracker/base/DecayCalculator.py
@@ -1,10 +1,23 @@
 class DecayCalculator:
     density_model = ""
+    altitude_temperature = 0
     current_altitude = 0
     def init(self,alt):
         print("hello")
-        self.current_altitude = alt;
+        self.current_altitude = alt
+        self.altitude_temperature = self.get_temp_at_altitude()
         self.get_density_model_at_alt()
 
     def get_density_model_at_alt(self):
         print(self.current_altitude)
+
+    def get_temp_at_altitude(self):
+        if(self.current_altitude < 5):
+            print("It's gonna crash!")
+        elif(self.current_altitude > 50):
+            print("Any day now")
+        elif(self.current_altitude > 1000):
+            print("I dunno")
+            return 5
+        else:
+            return 10
